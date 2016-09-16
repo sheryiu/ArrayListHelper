a225ea3601d1c3bd0049eb76bc3b40b2608749ff
Yiu Cheuk Lok <yiu.cheuk.lok@gmail.com>
2016-09-16 17:11:08 +0800

    Small new changes Version 1.0-beta-3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ArrayListHelper (Experimental)
-This library aims to help developers update their RecyclerView with correct animations. It makes use of `RecyclerView.Adapter` built-in functions `notifyItemInserted` and `notifyItemRemoved` to update respective items in the RecyclerView.
+This library aims to help developers update `RecyclerView` with correct animations when there are changes to the respective `ArrayList`. It makes use of `RecyclerView.Adapter` built-in functions `notifyItemInserted` and `notifyItemRemoved` to update respective items in the RecyclerView.
 ## Usage
 In order to use this library in Android Studio, add the following code in your project's gradle file.
 ```gradle
@@ -7,5 +7,18 @@ dependencies {
 	...
 	compile 'hk.sheryiu.arraylisthelper:arraylisthelper:1.0-beta-3'
 	...
+}
+```
+Then create a new instance of `ArrayListHelper` as a class variable in the class where you have your `RecyclerView`. Your instance should have the same type parameter as your `ArrayList`.
+
+You should initialize the instance in your `onCreate` method.
+```java
+private ArrayListHelper<String> myHelper;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    ...
+    myHelper = new ArrayListHelper<>(adapter, source);
+    ...
 }
 ```
--- arraylisthelper/src/main/java/hk/sheryiu/arraylisthelper/ArrayListHelper.java
+++ arraylisthelper/src/main/java/hk/sheryiu/arraylisthelper/ArrayListHelper.java
@@ -17,60 +17,79 @@ public class ArrayListHelper<T> {
 		setDataSource(adapter, source);
 	}
 
+	public ArrayListHelper(RecyclerView.Adapter adapter) {
+		this(adapter, null);
+	}
+
 	public ArrayListHelper() {
 		this(null, null);
 	}
 
-	public void setDataSource(RecyclerView.Adapter adapter, ArrayList<T> source) {
+	public void setAdapter(RecyclerView.Adapter adapter) {
 		this.adapter = adapter;
+	}
+
+	public void setDataSource(RecyclerView.Adapter adapter, ArrayList<T> source) {
+		setAdapter(adapter);
 		this.source = source;
 	}
 
 	public void updateDataSource(ArrayList<T> newsource) {
-		// match new list with old list
-		int[] matchingPos = new int[newsource.size()];
-		int totalNew = 0;
-		for (int i = 0; i < newsource.size(); i++) {
-			boolean matched = false;
-			T newitem = newsource.get(i);
-			for (int oldi = i - totalNew; oldi < source.size(); oldi++) {
-				T olditem = source.get(oldi);
-				if (newitem.equals(olditem)) {
-					matchingPos[i] = oldi + totalNew;
-					matched = true;
-					break;
+		if (adapter != null) {
+			if ((source == null || source.size() == 0) && newsource != null) {
+				source = new ArrayList<>(newsource);
+				adapter.notifyItemRangeInserted(0, source.size());
+			} else if ((newsource == null || newsource.size() == 0) && source != null) {
+				int size = source.size();
+				source.clear();
+				adapter.notifyItemRangeRemoved(0, size);
+			} else if (newsource != null && source != null) {
+				// match new list with old list
+				int[] matchingPos = new int[newsource.size()];
+				int totalNew = 0;
+				for (int i = 0; i < newsource.size(); i++) {
+					boolean matched = false;
+					T newitem = newsource.get(i);
+					for (int oldi = i - totalNew; oldi < source.size(); oldi++) {
+						T olditem = source.get(oldi);
+						if (newitem.equals(olditem)) {
+							matchingPos[i] = oldi + totalNew;
+							matched = true;
+							break;
+						}
+					}
+					if (!matched) {
+						matchingPos[i] = -1;
+						totalNew++;
+					}
 				}
-			}
-			if (!matched) {
-				matchingPos[i] = -1;
-				totalNew++;
-			}
-		}
 
-		for (int i = 0; i < matchingPos.length; i++) {
+				for (int i = 0; i < matchingPos.length; i++) {
 
-			// items inserted
-			if (matchingPos[i] == -1) {
-				source.add(i, newsource.get(i));
-				adapter.notifyItemInserted(i);
-			} else
-			// items deleted
-			if ((i > 0 && matchingPos[i] != matchingPos[i - 1] + 1) || (i == 0 && matchingPos[i] != 0)) {
-				for (int k = 0; k < matchingPos[i] - i; k++) {
-					source.remove(i);
+					// items inserted
+					if (matchingPos[i] == -1) {
+						source.add(i, newsource.get(i));
+						adapter.notifyItemInserted(i);
+					} else
+						// items deleted
+						if ((i > 0 && matchingPos[i] != matchingPos[i - 1] + 1) || (i == 0 && matchingPos[i] != 0)) {
+							for (int k = 0; k < matchingPos[i] - i; k++) {
+								source.remove(i);
+							}
+							adapter.notifyItemRangeRemoved(i, matchingPos[i] - i);
+						}
 				}
-				adapter.notifyItemRangeRemoved(i, matchingPos[i] - i);
-			}
-		}
-		// items deleted at the end
-		if (source.size() > newsource.size()) {
-			int size = source.size();
-			for (int k = 0; k < size - newsource.size(); k++) {
-				source.remove(newsource.size());
-			}
-			adapter.notifyItemRangeRemoved(newsource.size(), size - newsource.size());
-		}
+				// items deleted at the end
+				if (source.size() > newsource.size()) {
+					int size = source.size();
+					for (int k = 0; k < size - newsource.size(); k++) {
+						source.remove(newsource.size());
+					}
+					adapter.notifyItemRangeRemoved(newsource.size(), size - newsource.size());
+				}
 
-		// finish
+				// finish
+			}
+		}
 	}
 }
